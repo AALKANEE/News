1b19a1e425d054906e4fedcd497f260a620641a8
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-07-11 20:57:13 +0330

    finishing

--- js/ui.js
+++ js/ui.js
@@ -28,20 +28,40 @@ class UI {
     }
   }
 
+  //clear previous search results
+  clearResults(){
+    this.result.innerHTML="";
+  }
+
   // showing result into the html
   showNews(news) {
-      news.forEach((newsInfo) => {
-        console.log(newsInfo)
+    this.clearResults();
+    news.forEach((newsInfo) => {
+      console.log(newsInfo);
       this.result.innerHTML += `
                 <div class="col-md-4 mb-4>
                     <div class="card">
+                    <img src="${
+                      newsInfo.urlToImage
+                    }" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h3 class ="card-title text-center">${newsInfo.title.split('-',1)}</h3>
+                            <h3 class ="card-title text-center">${newsInfo.title.split(
+                              "-",
+                              1
+                            )}</h3>
                             <p class="card-text lead textto-info">news information: </p>
-                            <span class="badge badge-primary">Source: ${newsInfo.source.name}</span>
-                            <span class="badge badge-primary">Date & time: ${newsInfo.publishedAt.split('-',2)}</span>
+                            <span class="badge badge-primary">Source: ${
+                              newsInfo.source.name
+                            }</span>
+                            &nbsp;
+                            <span class="badge badge-primary">Date & time: ${newsInfo.publishedAt.split(
+                              "-",
+                              2
+                            )}</span>
                             </br>
-                            <a href="${newsInfo.url}" target="_blank" class="btn btn-primary btn-block">More...</a>
+                            <a href="${
+                              newsInfo.url
+                            }" target="_blank" class="btn btn-primary btn-block">More...</a>
                         </div>
                     </div>
                 </div>
